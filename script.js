e7348c5e9fa0816b6aacc99ce16470f564a28115
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-11-05 15:23:58 +0100

    parse csv

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 const ctx = document.getElementById('myChart');
-fetch('data/csvjson.json')
-  .then(response => response.json())
-  .then(_data => {
-    console.log(_data);
-    let labels = ['1','2'];
-    let countries = {};
+fetch('data/temperature.csv')
+  .then(response => response.text())
+  .then(text => {
+    let labels = new Set();
+    let places = {};
 
-    for (let entry of _data) {
+    let data = fromCSV(text);
+    for (let entry of data) {
       let key = entry.Country+'/'+entry.City;
-      let country = countries[key];
-      if (!country) {
-        countries[key] = [entry.AverageTemperatureFahr];
-      } else {
-        country.push(entry.AverageTemperatureFahr);
-      }
+      let country = places[key] || Object.assign(places, {[key]: []})[key];
+      let date = `${entry.year}-${entry.month}-${entry.day}`;
+      labels.add(date);
+
+      let value = parseFloat(entry.AverageTemperatureFahr);
+      country.push({x: date, y: convertFahrenheitToCelsius(value)});
     }
 
     let dataset = [];
-    for (let key in countries) {
-      dataset.push({label: key, data: countries[key]});
+    for (let key in places) {
+      dataset.push({
+        label: key,
+        hidden: true,
+        data: places[key].sort((a,b) => a.x.localeCompare(b.x)),
+        borderWidth: 1,
+      });
     }
+
+    dataset.sort((a,b) => a.label.localeCompare(b.label));
+    dataset[0].hidden = false;
+    dataset[1].hidden = false;
 
     console.log(dataset);
     new Chart(ctx, {
       type: 'line',
       data: {
-        labels: labels,
+        labels: Array.from(labels).sort(),
         datasets: dataset,
       },
       options: {
@@ -33,7 +42,45 @@ fetch('data/csvjson.json')
           legend: {
             position: 'right',
           }
+        },
+        scales: {
+          y: {
+            title: {
+              display: true,
+              text: 'Temperature in Celsius',
+            }
+          },
+          x: {
+            title: {
+              display: true,
+              text: 'Date',
+            }
+          },
         }
       }
     });
   });
+
+function fromCSV(text) {
+  // Split into lines and filter out empty lines
+  let lines = text.split('\n').filter(Boolean);
+  let objects = [];
+  let headers = removeQuotes(lines[0].split(','));
+  for (let i = 1; i < lines.length; i++) {
+    let line = removeQuotes(lines[i].split(','));
+    let entry = {};
+    for (let col in headers) {
+      entry[headers[col]] = line[col];
+    }
+    objects.push(entry);
+  }
+  return objects;
+}
+
+function removeQuotes(array) {
+  return array.map(string => string.replace(/["]+/g, ''));
+}
+
+function convertFahrenheitToCelsius(value) {
+  return (value - 32) / 1.8;
+}
