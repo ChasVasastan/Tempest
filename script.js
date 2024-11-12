7b981fbc6690011c3c27441a0d73ed83647d3b5d
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-11-12 10:40:05 +0100

    select start and end time

--- script.js
+++ script.js
@@ -1,29 +1,73 @@
 const ctx = document.getElementById('myChart');
+const config = {
+  type: 'line',
+  options: {
+    plugins: {
+      legend: {
+        position: 'right',
+      }
+    },
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: 'Temperature in Celsius',
+        }
+      },
+      x: {
+        type: 'time',
+        time: {
+          unit: 'month'
+        },
+        title: {
+          display: true,
+          text: 'Date',
+        }
+      },
+    }
+  }
+};
+
+const chart = new Chart(ctx, config);
+
+document.getElementById('date-start').addEventListener('change', (event) => {
+  config.options.scales.x.min = event.target.value;
+  chart.update();
+});
+document.getElementById('date-end').addEventListener('change', (event) => {
+  config.options.scales.x.max = event.target.value;
+  chart.update();
+});
+
+
 fetch('data/temperature.csv')
   .then(response => response.text())
   .then(text => {
-    let labels = new Set();
-    let places = {};
-
     let data = fromCSV(text);
+    let places = new Map();
+
     for (let entry of data) {
-      let key = entry.Country+'/'+entry.City;
-      let country = places[key] || Object.assign(places, {[key]: []})[key];
-      let date = `${entry.year}-${entry.month}-${entry.day}`;
-      labels.add(date);
+      places[entry.Country] ||= new Map();
+      places[entry.Country][entry.City] ||= [];
+      const date = new Date(entry.year, entry.month, entry.day);
 
       let value = parseFloat(entry.AverageTemperatureFahr);
-      country.push({x: date, y: convertFahrenheitToCelsius(value)});
+      places[entry.Country][entry.City].push({
+        x: date.getTime(),
+        y: convertFahrenheitToCelsius(value)
+      });
     }
 
     let dataset = [];
-    for (let key in places) {
-      dataset.push({
-        label: key,
-        hidden: true,
-        data: places[key].sort((a,b) => a.x.localeCompare(b.x)),
-        borderWidth: 1,
-      });
+    for (let country in places) {
+      for (let city in places[country]) {
+        dataset.push({
+          label: `${country}/${city}`,
+          hidden: true,
+          data: places[country][city].sort((a,b) => a.x - b.x),
+          borderWidth: 1,
+        });
+      }
     }
 
     dataset.sort((a,b) => a.label.localeCompare(b.label));
@@ -31,34 +75,8 @@ fetch('data/temperature.csv')
     dataset[1].hidden = false;
 
     console.log(dataset);
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Array.from(labels).sort(),
-        datasets: dataset,
-      },
-      options: {
-        plugins: {
-          legend: {
-            position: 'right',
-          }
-        },
-        scales: {
-          y: {
-            title: {
-              display: true,
-              text: 'Temperature in Celsius',
-            }
-          },
-          x: {
-            title: {
-              display: true,
-              text: 'Date',
-            }
-          },
-        }
-      }
-    });
+    config.data.datasets = dataset;
+    chart.update();
   });
 
 function fromCSV(text) {
